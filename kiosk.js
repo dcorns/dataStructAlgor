e33c39bee6b02ca9e20ca33fb6d50f9394e75d79
Dale Corns <drcorns@hotmail.com>
2014-08-31 02:04:22 -0700

    add films.txt read functionality

---
+++ kiosk.js
@@ -0,0 +1,18 @@
+'use strict';
+//Based on Data Structures and Algorithms with JavaScript, by Michael McMillian
+//video rental system example
+var kiosk = {};
+var fs = require('fs');
+var movies = "";
+
+//asynchronous call
+fs.readFile('films.txt','utf8',function(err, data){
+  if (err) throw err;
+  movies = data;
+  setList();
+});
+
+//Process callback
+function setList(){
+  console.log(movies);
+}
