82dca2e23094b976932a4dad58c9cb3d9909689a
Dale Corns <drcorns@hotmail.com>
2014-09-01 00:54:09 -0700

    more dinking around

--- kiosk.js
+++ kiosk.js
@@ -5,17 +5,27 @@ var kiosk = {};
 var fs = require('fs');
 var movieList = require('./list');
 
-//var movieList = new List();
-var movies = "";
-
-//asynchronous call
-fs.readFile('films.txt','utf8',function(err, data){
-  if (err) throw err;
-  movies = data;
-  setList();
+var movies = createArr('films.txt', function (stuff){
+  for (var i = 0; i < movies.length; i++){
+    movieList.append(stuff[i]);
+  }
 });
 
-//Process callback
-function setList(){
-  console.log(movies);
+function createArr(file) {
+  //asynchronous call
+  fs.readFile(file,'utf8',function(err, data){
+    if (err) throw err;
+    var arr = data.split('\n');
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].trim();
+    }
+    console.log(arr);
+    return arr;
+  });
+}
+
+function displayList(list){
+  for (list.front(); list.hasNext();){
+    console.log(list.next());
+  }
 }
